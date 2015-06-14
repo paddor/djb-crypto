74de89a6798ea4c05302b98f3e55eb765f39db69
Patrik Wenger <paddor@gmail.com>
2015-06-15 00:04:42 +0200

    rename Stream to StreamCipher and let all ciphers inherit from it

---
+++ lib/djb-crypto/stream_cipher.rb
@@ -0,0 +1,56 @@
+module DjbCrypto
+  class StreamCipher
+
+    # counter of the last usable block
+    MAX = 2**64 - 1
+
+    # Gets the first n bytes in the key stream.
+    # @return [Array<Integer>] next n bytes of key stream
+    def first_bytes(n)
+      raise "keystream wrap-around" if n/4 + (n%4==0 ? 0 : 1) > MAX
+      stream = stream_enumerator
+
+      # whole words
+      bytes = (n/4).times.map{ stream.next }.pack("V*").unpack("C*")
+
+      # remaining bytes
+      if (remaining = n % 4) != 0
+        bytes << stream.next.pack("V").byteslice(0, remaining).unpack("C*")
+      end
+
+      return bytes
+    end
+
+    # we start XOR-ing at block 1 (not 0) because the very first block is used for key
+    # generation for the authenticator
+    XOR_OFFSET = 1
+
+    # @param msg [String] message to XOR
+    # @return [String] result of XOR-ing
+    def ^(msg)
+      stream = stream_enumerator(XOR_OFFSET)
+
+      # whole words (4 bytes each)
+      x = msg.unpack("V*").map { |mb| mb ^ stream.next }.pack("V*")
+
+      # remaining bytes
+      if (remaining = msg.bytesize % 4) != 0
+        kstream_bytes = [stream.next].pack("V").unpack("C*")
+        msg_bytes = msg.byteslice(-remaining .. -1).unpack("C*")
+        x << msg_bytes.zip(kstream_bytes).map { |m,k| m ^ k }.pack("C*")
+      end
+
+      return x
+    end
+
+    private
+
+    def stream_enumerator(offset = 0)
+      Enumerator.new(MAX) do |stream|
+        offset.upto(MAX) do |counter|
+          block(counter).each { |word| stream << word }
+        end
+      end
+    end
+  end
+end
